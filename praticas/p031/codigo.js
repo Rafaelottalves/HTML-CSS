09d05b145714e41f6206f1f2ed6f6c593f2ed2d1
Rafaelottalves <rafaelott12345@gmail.com>
2022-10-08 07:24:24 -0300

    novos

---
+++ praticas/p031/codigo.js
@@ -0,0 +1,47 @@
+let entradaEmail = document.getElementById('t-email').value
+    let entradaSenha = document.getElementById('t-senha').value
+
+    let botao = document.getElementById('botao-submit')
+
+    let letras = []
+
+
+function entrou() {
+    for(let c = 0;c < entradaEmail.length;c++) {
+        letras.push(entradaEmail[c])
+    }
+
+    if(entradaEmail.length == '' || letras.indexOf('@') === -1 || entradaSenha == '' || entradaSenha.length < 8) {
+        botao.style.cssText = `
+            display: none;
+        `
+    }
+}
+
+function saiu() {
+    botao.style.cssText = `
+            display: block;
+        `
+}
+
+
+
+
+/* function verificar() {
+    let entradaEmail = document.getElementById('t-email').value
+    let entradaSenha = document.getElementById('t-senha').value
+
+    let botao = document.getElementById('botao-submit')
+
+    let letras = []
+
+    for(let c = 0;c < entradaEmail.length;c++) {
+        letras.push(entradaEmail[c])
+    }
+
+    if(entradaEmail.length == '' || letras.indexOf('@') === -1 || entradaSenha == '' || entradaSenha.length < 8) {
+    }
+    else {
+    }
+}
+ */
