cf7dcc079ef3a731d87bb0f25daffd712234d130
Rafael Ott Alves <105594751+Rafaelottalves@users.noreply.github.com>
2023-10-15 17:43:34 -0300

    Arsha site - menu (atualização 02)

--- praticas/p34 - Arsha/javascript/drop-down.js
+++ praticas/p34 - Arsha/javascript/drop-down.js
@@ -1,5 +1,23 @@
 let dropDownList = document.querySelector('.drop-down-list')
+let containerDrop = document.querySelector('.container-drop')
 
-dropDownList.onclick = function() {
-    dropDownList.style.height = '230px'
+let containerDeepDrop = document.querySelector('.container-deep-drop')
+let deepDropDownList = document.querySelector('.deep-drop-down-list')
+
+containerDrop.onclick = function() {
+    dropDownList.classList.toggle("showDrop")
+
+    setTimeout(() => {
+        dropDownList.classList.toggle("visible")
+    })
+}
+
+containerDeepDrop.onclick = function() {
+    deepDropDownList.classList.toggle('showDrop')
+
+    setTimeout(() => {
+        dropDownList.classList.toggle('new-visible')
+
+        deepDropDownList.classList.toggle('visible')
+    })
 }
