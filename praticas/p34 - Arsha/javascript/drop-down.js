a08c2e34f8e51d82e07e7410b18bdad0d1528ff1
Rafael Ott Alves <105594751+Rafaelottalves@users.noreply.github.com>
2023-10-16 21:02:08 -0300

    Arsha - site menu(finalização), add section home

--- praticas/p34 - Arsha/javascript/drop-down.js
+++ praticas/p34 - Arsha/javascript/drop-down.js
@@ -1,11 +1,13 @@
 let dropDownList = document.querySelector('.drop-down-list')
 let containerDrop = document.querySelector('.container-drop')
+let dropOption = document.querySelector('.container-drop > a')
 
 let containerDeepDrop = document.querySelector('.container-deep-drop')
 let deepDropDownList = document.querySelector('.deep-drop-down-list')
 
 containerDrop.onclick = function() {
     dropDownList.classList.toggle("showDrop")
+    dropOption.classList.toggle('change-color')
 
     setTimeout(() => {
         dropDownList.classList.toggle("visible")
