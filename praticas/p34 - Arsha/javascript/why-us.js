0d9019acae734bb461efbb3a2f3bb4dbba5976f5
Rafael Ott Alves <105594751+Rafaelottalves@users.noreply.github.com>
2023-10-21 18:11:14 -0300

    Arsha site - why-us section

--- praticas/p34 - Arsha/javascript/why-us.js
+++ praticas/p34 - Arsha/javascript/why-us.js
@@ -2,14 +2,17 @@ let listOfItems = document.querySelectorAll('.why-us-section-list > li')
 
 listOfItems.forEach((item) => {
     item.onclick = function() {
+        item.classList.toggle('showCard')
+        item.id = 'a'
+
         listOfItems.forEach((itemList) => {
-            if(itemList.className == 'showCard') {
-                itemList.classList.toggle('hideCard')
+            if(itemList.className == 'showCard' && itemList.id !== 'a') {
+                itemList.classList.remove('showCard')
+            }
+
+            else {
+                itemList.id = ''
             }
         })
-
-        item.classList.toggle('showCard')
     }
 })
-
-// todos menos o que ta sendo clicado
